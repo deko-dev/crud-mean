396af76f0b21aab19b9b171efd78fbe796c426b4
decodev-front <deiser1999@gmail.com>
2020-08-20 19:27:58 -0500

    Subiendo a HEROKU

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductoComponent } from './pages/producto/producto.component';
